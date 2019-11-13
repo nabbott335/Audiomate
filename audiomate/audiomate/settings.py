22ef9d7bc7b692b475fbb7841788748069adc1e8
NJA <39605984+nabbott335@users.noreply.github.com>
2019-11-13 12:33:16 -0500

    Update settings.py

--- audiomate/audiomate/settings.py
+++ audiomate/audiomate/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'audiomate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'audiomate',
-		'USER': 'root',
-		'PASSWORD': '',
-		'HOST':'localhost',
-		'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
