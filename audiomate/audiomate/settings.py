387167cb89eeb1e593331f3968d640fa2b0427a3
Nick Abbott <nabbott335@gmail.com>
2019-11-13 18:15:07 -0500

    Update settings.py

--- audiomate/audiomate/settings.py
+++ audiomate/audiomate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Rests his head on a pillow made of concrete'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
